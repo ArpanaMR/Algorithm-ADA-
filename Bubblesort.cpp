a7fe7b1bc3033e234441ab58eba34ebd26b463e6
ArpanaMR <56809055+ArpanaMR@users.noreply.github.com>
2020-02-27 11:56:25 +0530

    Update Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,78 +1,72 @@
-#include<bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n) 
-{ 
-   int i, j,temp;
-   bool swapped; 
-   swapped = false; 
+void rotate_binary(int arr[],int n, int key)
+{
+    int midi;
+    int begi=0,endi=n-1;
+    midi=(begi+endi)/2;
 
- for (i = 0; i < n-1; i++) 
-   { 
+    while(begi!=endi)
+    {
+
+        if(arr[midi]==key)
+        {
+            cout<<"element found at position "<<(midi+1)<<endl;
+            return;
+        }
+
+        // right sorted
+        else if(arr[midi]<key && arr[midi]<=arr[endi] && key<=arr[endi])
+        {
+            begi=midi+1;
+        }
+
+        
+        else if(arr[midi]>key && arr[begi]<=arr[midi] && key>=arr[begi])
+        {
+            endi=midi-1;
+        }
+
      
-     for (j = 0; j < n-i-1; j++) 
-     { 
-        if (arr[j] > arr[j+1]) 
-        { 
-           temp=arr[j];
-           arr[j]=arr[j+1];
-           arr[j+1]=temp;
-        } 
-     } 
-  
-      
-     if (swapped == false) 
-        break; 
-   }
-   cout<<"Sorted Array is"<<endl;
-   
-   for(int i=0;i<n;i++)
-   cout<<arr[i]<<endl;
-} 
-  
+        else if(arr[begi]>=arr[midi] && (key>=arr[begi] || key<=arr[midi]))
+        {
+            endi=midi;
+        }
 
-int main() 
-{ 
-    int n,arr[n];clock_t starttime,endtime;
+        
+        else
+        {
+            begi=midi;
+        }
+
+        midi=(begi+endi)/2;
+
+    }
+    if(key==arr[midi])
+        cout<<"Element found at position "<<(midi+1)<<endl;
+    else
+        cout<<"Element not found"<<endl;
+}
+
+int main()
+{
+    {int n;
+    int arr[n];
+    
+    int key;
+    
     cout<<"Enter array size"<<endl;
     cin>>n;
     cout<<"Enter elements"<<endl;
-
+    
     for(int i=0;i<n;i++)
     cin>>arr[i];
     
+    cout<<"Enter key"<<endl;
+    cin>>key;
     
-   starttime = clock(); 
-bubbleSort(arr, n); 
-endtime = clock();
-    double time_taken = double(endtime - starttime) / double(CLOCKS_PER_SEC); 
-    cout << "Time taken by program is : " << fixed  
-         << time_taken << setprecision(10)<<endl; 
-    
-    return 0; 
-} 
-/*Enter array size
-5
-Enter elements
-0 -9 177 8 100
-Sorted Array is
--9
-0
-8
-100
-177
-Time taken by program is : 0.000034
+    rotate_binary(arr,n,key);
+    return 0;
+}
 
-Enter array size
-5
-Enter elements
--9 0 8 100 177
-Sorted Array is
--9
-0
-8
-100
-177
-Time taken by program is : 0.000016
-*/
